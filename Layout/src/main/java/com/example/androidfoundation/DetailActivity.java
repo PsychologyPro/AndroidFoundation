13ca42239f88449afd4bf7710cb0cfcbcebe4ed9
ymh8077 <807749026@qq.com>
2020-11-12 15:39:41 +0800

    添加详情页面

--- Layout/src/main/java/com/example/androidfoundation/DetailActivity.java
+++ Layout/src/main/java/com/example/androidfoundation/DetailActivity.java
@@ -3,13 +3,12 @@ package com.example.androidfoundation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 }
